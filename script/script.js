4a43a43048f40d4a7bf77a53b5596e8b38f35a06
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-03-07 20:38:03 -0300

    Defense Planet

--- script/script.js
+++ script/script.js
@@ -66,24 +66,27 @@ function game() {
         //controling player start´s
         window.addEventListener('keydown',function(e) {
             let key = e.keyCode;
-            switch (key){
-                case 37:
-                    dirPlayerx = -1 ;
-                    break ;
-                case 38:
-                    dirPlayery = -1 ;
-                    break ;
-                case 39:
-                    dirPlayerx = 1 ;
-                    break ;
-                case 40:
-                    dirPlayery = 1 ;
-                    break ;
+            if (key == 37) {
+                dirPlayerx = -1 ;
+            }else if (key == 40) {
+                dirPlayery = 1 ;
+            }else if (key == 39) {
+                dirPlayerx = 1 ;
+            }else if (key == 38) {
+                dirPlayery = -1 ;
             }
+            if (key == 13) {
+                shot() ;
+            } ;
         }) ;
-        window.addEventListener('keyup',function() {
-            dirPlayerx = 0 ;
-            dirPlayery = 0 ;
+        window.addEventListener('keyup',function(e) {
+            let key = e.keyCode ;
+            if (key == 37 || key == 39) {
+                dirPlayerx = 0 ;
+            } ;
+            if (key == 38 || key == 40) {
+                dirPlayery = 0 ;
+            } ;
         }) ;
         //controling player end´s
         startControl = false ;
@@ -91,12 +94,13 @@ function game() {
     }
     if (playing) {
         controlPLayer() ;
+        controlShots() ;
     } ;
     frame = requestAnimationFrame(game) ;
 } ;
 
 function start() {
-    posPlayerx = 50 ;
+    posPlayerx = 45 ;
     posPlayery = 80 ;
     velPLayer = 0.6 ;
     dirPlayerx = 0 ; 
@@ -106,9 +110,42 @@ function start() {
     game() ;
 } ;
 
+function shot() {
+    let div = document.createElement('div') ;
+    let att1 = document.createAttribute('class') ;
+    att1.value = 'shot' ;
+    div.setAttributeNode(att1) ;
+    document.querySelector('#shots').appendChild(div) ;
+    let lastDiv = document.querySelector('#shots').lastChild ;
+    lastDiv.style.top = `${posPlayery + 1}%` ;
+    lastDiv.style.left = `${posPlayerx + (document.querySelector('#player').offsetWidth/window.innerWidth*100)/2}%` ;
+} ;
+
 function controlPLayer() {
     posPlayerx += velPLayer * dirPlayerx ;
     posPlayery += velPLayer * dirPlayery ;
+    if (posPlayerx >= 100 - document.querySelector('#player').offsetWidth/window.innerWidth*100) {
+        posPlayerx = 100 - document.querySelector('#player').offsetWidth/window.innerWidth*100 ;
+    } ;
+    if (posPlayerx <= 0) {
+        posPlayerx = 0 ;
+    } ;
+    if (posPlayery >= Math.ceil(100 - document.querySelector('#player').offsetHeight/window.innerHeight*100)) {
+        posPlayery = Math.ceil(100 - document.querySelector('#player').offsetHeight/window.innerHeight*100) ;
+    } ;
+    if (posPlayery <= 0) {
+        posPlayery = 0 ;
+    } ;
     document.querySelector('#player').style.left = `${posPlayerx}%` ;
     document.querySelector('#player').style.top = `${posPlayery}%` ;
 } ;
+
+function controlShots(){
+    let shots = document.querySelectorAll('#shots .shot') ;
+    shots.forEach(item => {
+        item.style.top = parseInt(item.style.top.replace('%','')) -1 + '%' ;
+        if (parseInt(item.style.top.replace('%','')) < -2) {
+            item.remove() ;
+        }
+    }) ;
+}
